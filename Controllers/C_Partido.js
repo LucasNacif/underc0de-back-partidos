aae09661387a7617a3760dc62bb350035bc07d48
LucasNacif <130097025+LucasNacif@users.noreply.github.com>
2024-10-31 15:56:08 -0300

    Feature/obtener partido por estado

--- Controllers/C_Partido.js
+++ Controllers/C_Partido.js
@@ -26,6 +26,23 @@ exports.obtenerPartido = async (req,res) =>{
         return res.status(400).json({ msg: "Error al buscar los partidos: "});
     }
 }
+exports.obtenerPartidosPorEstado = async (req , res) =>{
+    try {
+       const estado = req.params.estado;
+
+       // Validacion de estados permitidos
+       const estadosPermitidos = ["activo", "cancelado", "reprogramado"];
+       if (!estadosPermitidos.includes(estado)) {
+           return res.status(400).json({ error: 'Estado inválido' });
+       }
+       const partidos = await Partido.findAll({ where: { estado } });
+
+       res.json({ Partidos: partidos });
+
+    } catch (error) {
+        return res.status(400).json({ msg: "Error al buscar los partidos por estado"});
+    }
+}
 //metodo de modificar
 //metodo de eliminar
 //metodo de baja
