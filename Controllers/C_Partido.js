6d7181fcae5d15681ab95cdee9fd7d59a19fc7e1
LucasNacif <130097025+LucasNacif@users.noreply.github.com>
2024-10-31 15:33:01 -0300

    Feature/ EndPoint obtenerPartido

--- Controllers/C_Partido.js
+++ Controllers/C_Partido.js
@@ -1,8 +1,6 @@
 const Partido = require("../models/Partido");
 
 exports.crearPartido = async (req, res) => {
-
-    console.log(req.body);
     try {
     const PartidoParaCrear = { fecha, hora, ubicacion, limite_jugadores, precio, comentario, estado, idAdmin } = req.body;
         await Partido.create({...PartidoParaCrear});
@@ -17,7 +15,15 @@ exports.obtenerPorID = async (req, res) => {
         const partidoEncontrado = await Partido.findByPk(idPartido);
         return res.status(200).json({partidoEncontrado});
     } catch (error) {
-        
+        return res.status(400).json({ msg: "Error al buscar el partido: ", err });
+    }
+}
+exports.obtenerPartido = async (req,res) =>{
+    try {
+        const partidos = await Partido.findAll();
+        return res.status(200).json({partidos});
+    } catch (error) {
+        return res.status(400).json({ msg: "Error al buscar los partidos: "});
     }
 }
 //metodo de modificar
--- Routes/R_Partido.js
+++ Routes/R_Partido.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const controller = require("../Controllers/C_Partido");
 
 router.post('/crear', controller.crearPartido);
+router.get('/obtener', controller.obtenerPartido);
 router.get('/obtenerPorId', controller.obtenerPorID);
 //ruta para modificar
 //ruta para crear
