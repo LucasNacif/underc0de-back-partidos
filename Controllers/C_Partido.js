3296769e2b00e4251939da7bd7eb868b5b002a6e
LucasNacif <130097025+LucasNacif@users.noreply.github.com>
2024-11-24 10:42:20 -0300

    Feature/ eliminar partido

--- Controllers/C_Partido.js
+++ Controllers/C_Partido.js
@@ -2,45 +2,54 @@ const Partido = require("../models/Partido");
 
 exports.crearPartido = async (req, res) => {
     try {
-    const PartidoParaCrear = { fecha, hora, ubicacion, limite_jugadores, precio, comentario, estado, idAdmin } = req.body;
-        await Partido.create({...PartidoParaCrear});
-        return res.status(200).json({ msg: "Partido creado existosamente"});
+        const PartidoParaCrear = { fecha, hora, ubicacion, limite_jugadores, precio, comentario, estado, idAdmin } = req.body;
+        await Partido.create({ ...PartidoParaCrear });
+        return res.status(200).json({ msg: "Partido creado existosamente" });
     } catch (err) {
         return res.status(400).json({ msg: "Error al crear el partido: ", err });
     }
 };
+exports.eliminarPartido = async (req, res) => {
+    try {
+        const idPartido = req.params.id;
+        await Partido.destroy({ where: { idPartido: idPartido } });
+        return res.status(200).json({ msg: "Partido eliminado existosamente" });
+    } catch (err) {
+        return res.status(400).json({ msg: "Error al eliminar el partido: ", err });
+    }
+}
 exports.obtenerPorID = async (req, res) => {
     try {
-        const {idPartido} = req.body;
+        const { idPartido } = req.body;
         const partidoEncontrado = await Partido.findByPk(idPartido);
-        return res.status(200).json({partidoEncontrado});
+        return res.status(200).json({ partidoEncontrado });
     } catch (error) {
         return res.status(400).json({ msg: "Error al buscar el partido: ", err });
     }
 }
-exports.obtenerPartido = async (req,res) =>{
+exports.obtenerPartido = async (req, res) => {
     try {
         const partidos = await Partido.findAll();
-        return res.status(200).json({partidos});
+        return res.status(200).json({ partidos });
     } catch (error) {
-        return res.status(400).json({ msg: "Error al buscar los partidos: "});
+        return res.status(400).json({ msg: "Error al buscar los partidos: " });
     }
 }
-exports.obtenerPartidosPorEstado = async (req , res) =>{
+exports.obtenerPartidosPorEstado = async (req, res) => {
     try {
-       const estado = req.params.estado;
+        const estado = req.params.estado;
 
-       // Validacion de estados permitidos
-       const estadosPermitidos = ["activo", "cancelado", "reprogramado"];
-       if (!estadosPermitidos.includes(estado)) {
-           return res.status(400).json({ error: 'Estado inválido' });
-       }
-       const partidos = await Partido.findAll({ where: { estado } });
+        // Validacion de estados permitidos
+        const estadosPermitidos = ["activo", "cancelado", "reprogramado"];
+        if (!estadosPermitidos.includes(estado)) {
+            return res.status(400).json({ error: 'Estado inválido' });
+        }
+        const partidos = await Partido.findAll({ where: { estado } });
 
-       res.json({ Partidos: partidos });
+        res.json({ Partidos: partidos });
 
     } catch (error) {
-        return res.status(400).json({ msg: "Error al buscar los partidos por estado"});
+        return res.status(400).json({ msg: "Error al buscar los partidos por estado" });
     }
 }
 
--- Routes/Partido.routes.js
+++ Routes/Partido.routes.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const controller = require("../Controllers/C_Partido");
 
 router.post('/crear', controller.crearPartido);
+router.delete('/eliminar/:id', controller.eliminarPartido);
 router.get('/obtener', controller.obtenerPartido);
 router.get('/obtenerPorId', controller.obtenerPorID);
 router.get('/obtener/:estado', controller.obtenerPartidosPorEstado);
