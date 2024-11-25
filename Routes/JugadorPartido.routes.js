c89c31572375d556e8f12ae2463fe691e7056102
LucasNacif <130097025+LucasNacif@users.noreply.github.com>
2024-11-25 19:15:30 -0300

    fix/ preferencia mp

--- Routes/JugadorPartido.routes.js
+++ Routes/JugadorPartido.routes.js
@@ -4,6 +4,5 @@ const controller = require("../Controllers/C_JugadorPartido");
 
 router.delete('/eliminar_jugador_partido', controller.eliminarJugadorDePartido);
 router.get('/:idPartido/jugadores', controller.obtenerJugadoresDePartido);
-router.post("/inscribir", controller.inscribirJugador);
 
 module.exports = router;
