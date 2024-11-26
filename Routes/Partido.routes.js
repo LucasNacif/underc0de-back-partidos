5f694e4b9dd03f469f2ace6736d2b6cf22bf4e07
LucasNacif <130097025+LucasNacif@users.noreply.github.com>
2024-11-26 10:21:25 -0300

    Fix/ editar partido

--- Routes/Partido.routes.js
+++ Routes/Partido.routes.js
@@ -8,7 +8,7 @@ router.get("/obtener", controller.obtenerPartido);
 router.get("/obtenerPorId", controller.obtenerPorID);
 router.get("/obtener/:estado", controller.obtenerPartidosPorEstado);
 router.delete("/eliminar/:id", controller.eliminarPartido);
-router.post("/cancelar/:id", controller.cancelarPartido);
+router.put("/editar/:id", controller.editarPartido);
 
 
 module.exports = router;
